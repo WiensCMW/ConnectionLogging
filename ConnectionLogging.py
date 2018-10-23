89473092e4e3ffaac28436dbfd1c62e7b9f1ea80
Cornelius Wiens <cmw1334@gmail.com>
2018-10-22 21:38:15 -0500

    Started testing with function

--- ConnectionLogging.py
+++ ConnectionLogging.py
@@ -1,1 +1,17 @@
-print("Hello World!")
+import os
+
+# https://stackoverflow.com/questions/26468640/python-function-to-test-ping
+def check_ping():
+    hostname = "google.com"
+    response = os.system("ping -c 1 " + hostname)
+    # and then check the response...
+    if response == 0:
+        pingstatus = "Network Active"
+    else:
+        pingstatus = "Network Error"
+
+    return pingstatus
+
+pingResults = check_ping()
+
+print(pingResults)
