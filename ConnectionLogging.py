f697f8ec41d41ecde28a6fe70bc36b6db7e03ad2
Cornelius Wiens <cmw1334@gmail.com>
2018-10-23 20:54:30 -0500

    Tested with try/catch.
    Also committing before trying new ping test

--- ConnectionLogging.py
+++ ConnectionLogging.py
@@ -1,14 +1,20 @@
 import os
 
+# Variables
+hostname = "google.com"
+
 # https://stackoverflow.com/questions/26468640/python-function-to-test-ping
 def check_ping():
-    hostname = "google.com"
-    response = os.system("ping -c 1 " + hostname)
-    # and then check the response...
-    if response == 0:
-        pingstatus = "Network Active"
-    else:
-        pingstatus = "Network Error"
+    pingstatus = ""
+    try:
+        response = os.system("ping -c 1 " + hostname)
+        # and then check the response...
+        if response == 0:
+            pingstatus = "Network Active"
+        else:
+            pingstatus = "Network Error"
+    except Exception as e:
+        pingstatus = e
 
     return pingstatus
 
