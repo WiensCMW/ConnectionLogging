0b2be90894bf42d37649d1324ea52af23322fb6e
Cornelius Wiens <cmw1334@gmail.com>
2018-10-24 20:25:08 -0500

    Changed script to function on 3 second timer.
    Script now only continues if it can ping the router.

--- ConnectionLogging.py
+++ ConnectionLogging.py
@@ -1,12 +1,13 @@
 import os
 import subprocess
 import datetime
+import time
 
 # list of addresses to use in the ping test
-host_list = ["www.google.com", "www.yahoo.com", "www.xbox.com"]
+host_list = ["www.google.com", "www.yahoo.com"]
 
 # log directory
-log_dir = "/Users/corneliuswienz/Documents/log testing"
+log_dir = "/log testing"
 
 # ping test function that suppresses the ping results output
 # https://stackoverflow.com/questions/28769023/get-output-of-system-ping-without-printing-to-the-console
@@ -24,19 +25,23 @@ def pingtest(host_name):
 
     return results
 
-# loop through host list and perform ping test until one succeeds
-pingtest_passed = False
-i = 0
-while i < len(host_list):
-    if (pingtest(host_list[i])) is True:
-        pingtest_passed = True
-        break
-    i += 1
+while True:
+    # loop through host list and perform ping test until one succeeds
+    pingtest_passed = False
 
-# handle failed ping
-if pingtest_passed is False:
-    # log results to file
-    with open(log_dir + "/ping_fail_log.txt", "a") as myfile:
-        myfile.write(str(datetime.datetime.now()) + "\r\n")
+    # check if router is reachable and only continue if it is.
+    if pingtest("192.168.1.1") is True:
+        i = 0
+        while i < len(host_list):
+            if (pingtest(host_list[i])) is True:
+                pingtest_passed = True
+                break
+            i += 1
 
-# print(datetime.datetime.now())
+        # handle failed ping
+        if pingtest_passed is False:
+            # log results to file
+            with open(log_dir + "/ping_fail_log.txt", "a") as myfile:
+                myfile.write(str(datetime.datetime.now()) + "\r\n")
+
+        time.sleep(3)
